c6c719ea5d94f2ff3f3dc41ac37d375dabcc2b7e
JB Orozco <35300800+jborozco@users.noreply.github.com>
2023-05-17 12:13:03 +0200

    Fill div on click + hover
    Need improvement: the first pixel clicked doesn't change.

--- script.js
+++ script.js
@@ -5,16 +5,55 @@ let canvas = document.getElementById('canvas');
 function FillCanvas(PxlNb) {
     for (let i = PxlNb; i > 0; i--) {
         let pixel = document.createElement("div");
-        pixel.className = "pixel";
+        pixel.className = `pixel pixel-${i}`;
         canvas.appendChild(pixel);
-        console.log(i);
-
     }
 }
 
 FillCanvas(16);
 
 //2. On click + hover on a div, the squares turn black
+
+let pixels = document.getElementsByClassName('pixel');
+let pixelsArray = [...pixels];
+
+
+
+//listen if the mouse is down
+let mouseDown = 0;
+document.body.onmousedown = function () {
+    ++mouseDown;
+}
+document.body.onmouseup = function () {
+    --mouseDown;
+}
+
+//2.1 switch color function on click
+function darkPixel(event) {
+    if (mouseDown) {
+        event.target.classList.add("dark-pixel");
+    }
+}
+
+
+pixelsArray.forEach(item => {
+    item.addEventListener('mouseover', event => {
+        if (mouseDown == true) {
+            darkPixel(event);
+        }
+    })
+    item.addEventListener('click', event => {
+        darkPixel(event);
+
+    })
+});
+
+
+
+
+
+
+
 
 
 //3. Choose the number of squares in the canvas and reset the grid, 
