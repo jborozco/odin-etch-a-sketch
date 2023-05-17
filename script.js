04df40820fa997d663995f7f9c49dcc43e547212
JB Orozco <35300800+jborozco@users.noreply.github.com>
2023-05-17 10:29:09 +0200

    Write pseudo code

---
+++ script.js
@@ -0,0 +1,21 @@
+//1. Set 4x4 squares in a canvas
+
+
+//2. On click + hover on a div, the squares turn black
+
+
+//3. Choose the number of squares in the canvas and reset the grid, 
+//add a limit of 64px
+
+
+//4. Add a button to clear/reset the canvas
+
+
+//5. Add a switch to a new behavior that "erase" by returning the div to white
+
+
+//6. Add a switch to a new behavior that color the div with random 
+//colors
+
+
+//7. Add a switch to a new behavior that color the div that add 10% black on click + hover  
